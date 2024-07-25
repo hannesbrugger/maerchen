87fa2f808db5c931db111e4ff07eb07d7936b1c1
hannesbrugger <hannes.brugger.007@gmail.com>
2024-07-25 12:00:29 +0700

    feat: buttons

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,11 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			huntergreen: '#34602dff',
+			magnolia: '#ece5f0ff',
+			glaucous: '#7880b5ff',
+		},
 	},
 	plugins: [],
 }
