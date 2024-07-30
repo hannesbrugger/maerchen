8c5c8362ea3e22fba25ec440bbd20c8887dcf883
hannesbrugger <hannes.brugger.007@gmail.com>
2024-07-30 14:33:15 +0700

    feat: rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,25 +1,21 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
-import sanitizeHtml from 'sanitize-html';
-import MarkdownIt from 'markdown-it';
-const parser = new MarkdownIt();
+import { ConvertDateTime } from '../components/ConvertDateTime';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const blog = await getCollection('blog');
 	return rss({
-		title: SITE_TITLE,
-		stylesheet: '/rss.xsl',
-		description: SITE_DESCRIPTION,
+		stylesheet: '/rss/styles.xsl',
+		title: 'Ullis Märchen',
+		description: 'Ein Blog über meine Events',
 		site: context.site,
-		trailingSlash: false,
-		items: posts.map((post) => ({
-			...post.data,
+		items: blog.map((post) => ({
+			title: post.data.Titel,
+			pubDate: "2019-12-29 18:00:00 +0100",
+			description: post.data.Beschreibung,
+			// Compute RSS link from post `slug`
+			// This example assumes all posts are rendered as `/blog/[slug]` routes
 			link: `/blog/${post.slug}/`,
-			content: sanitizeHtml(parser.render(post.body), {
-				allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
-			}),
-			...post.data,
 		})),
 	});
 }
